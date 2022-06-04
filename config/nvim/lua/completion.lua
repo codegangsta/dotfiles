c9393a15ef0bb58bc9f7a37963998577e158f8c1
Jeremy Saenz <jeremy.saenz@gmail.com>
2022-06-04 14:29:08 -0700

    Cleaned up completion.lua

--- config/nvim/lua/completion.lua
+++ config/nvim/lua/completion.lua
@@ -9,20 +9,14 @@
         mode = 'symbol', -- show only symbol annotations
         maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
 
-        -- The function below will be called before any actual modifications from lspkind
-        -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
         before = function (_, vim_item)
           return vim_item
         end
       })
     },
     snippet = {
-      -- REQUIRED - you must specify a snippet engine
       expand = function(args)
         vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-        -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-        -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-        -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
       end,
     },
     window = {
@@ -34,15 +28,18 @@
       ['<C-f>'] = cmp.mapping.scroll_docs(4),
       ['<C-Space>'] = cmp.mapping.complete(),
       ['<C-e>'] = cmp.mapping.abort(),
-      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+      ['<CR>'] = cmp.mapping.confirm({ select = true }),
+      ['<TAB>'] = cmp.mapping.confirm({ select = true }),
     }),
     sources = cmp.config.sources({
-      { name = 'nvim_lsp_signature_help' },
       { name = 'nvim_lsp' },
       { name = 'vsnip' },
     }, {
       { name = 'buffer' },
-    })
+    }),
+    experimental = {
+      ghost_text = true
+    },
   })
 
   -- Set configuration for specific filetype.
@@ -75,7 +72,7 @@
 
 -- Show line diagnostics automatically in hover window
 vim.o.updatetime = 250
-vim.cmd [[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
+vim.cmd [[autocmd CursorHold * lua vim.diagnostic.open_float(nil, {focus=false})]]
 
 -- Setup lspconfig.
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
@@ -94,9 +91,6 @@ config.gopls.setup {
       staticcheck = true,
     },
   },
-  init_options = {
-    usePlaceholders = true,
-  }
 }
 
 config.sumneko_lua.setup{
