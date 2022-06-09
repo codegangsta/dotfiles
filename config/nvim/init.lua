d0d26af31863303b27c9c3ba025605ddaa7179a2
Jeremy Saenz <jeremy.saenz@gmail.com>
2022-06-09 12:28:09 -0700

    Remove window_picker in nvim tree

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -51,7 +51,10 @@ require('nvim-tree').setup{
   },
   actions = {
     open_file = {
-      quit_on_open = true
+      quit_on_open = true,
+      window_picker = {
+        enable = false
+      },
     }
   },
 }
