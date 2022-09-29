7eb523d6ab27e8519cac689ca55f2ff3cb7cda1d
Jeremy Saenz <jeremy.saenz@gmail.com>
2022-09-28 21:28:55 -0700

    kubernetes completion

--- config/nvim/lua/completion.lua
+++ config/nvim/lua/completion.lua
@@ -35,8 +35,8 @@ cmp.setup({
 
   sources = cmp.config.sources({
     { name = 'nvim_lsp_signature_help' },
-    { name = 'luasnip' },
     { name = 'nvim_lsp' },
+    { name = 'luasnip' },
     { name = 'spell' },
   }),
 
