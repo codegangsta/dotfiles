d46c6c5e89942d372fadc55b2519015b7c8f6f29
Jeremy Saenz <jeremy.saenz@gmail.com>
2023-04-14 10:29:37 -0700

    Updated dependencies and some env variables

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -20,6 +20,12 @@ set -gx DOTFILES $HOME/.dotfiles
 set -gx KO_DOCKER_REPO ghcr.io/codegangsta
 set -gx TF_VAR_do_token op://codegangsta/do_terraform_token/password
 
+set -gx SCP_US_WEST nats://127.0.0.1:14222,nats://127.0.0.1:14223,nats://127.0.0.1:14224,nats://127.0.0.1:14225,nats://127.0.0.1:14226
+set -gx SCP_US_CENTRAL nats://127.0.0.1:15222,nats://127.0.0.1:15223,nats://127.0.0.1:15224
+set -gx SCP_US_EAST nats://127.0.0.1:16222,nats://127.0.0.1:16223,nats://127.0.0.1:16224
+set -gx SCP_US_APAC nats://127.0.0.1:17222,nats://127.0.0.1:17223,nats://127.0.0.1:17224
+set -gx SCP_US_EU_CENTRAL nats://127.0.0.1:18222,nats://127.0.0.1:18223,nats://127.0.0.1:18224
+
 fish_add_path $HOME/.dotfiles/bin
 fish_add_path $HOME/.local/bin
 fish_add_path $HOME/.cargo/bin
