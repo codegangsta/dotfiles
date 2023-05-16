3951b180f8ab060c56a567f0c6323a245cf0d8f1
Jeremy Saenz <jeremy.saenz@gmail.com>
2023-05-16 12:27:10 -0700

    Actually add copilot back

--- config/nvim/lua/custom/plugins.lua
+++ config/nvim/lua/custom/plugins.lua
@@ -121,6 +121,10 @@ local plugins = {
     event = { "CmdlineEnter" },
     ft = { "go", 'gomod' },
     build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+  },
+  {
+    "github/copilot.vim",
+    lazy = false,
   }
 }
 
