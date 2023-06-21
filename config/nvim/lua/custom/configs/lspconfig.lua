c8212574c6ed00bf28f2201e26c8d4397fb2a66c
Jeremy Saenz <jeremy.saenz@gmail.com>
2023-06-21 15:29:45 -0700

    Update deps

--- config/nvim/lua/custom/configs/lspconfig.lua
+++ config/nvim/lua/custom/configs/lspconfig.lua
@@ -66,8 +66,8 @@ null_ls.setup({
   on_attach = require("lsp-format").on_attach,
   sources = {
     null_ls.builtins.formatting.prettierd,
-    null_ls.builtins.code_actions.eslint_d,
-    null_ls.builtins.diagnostics.eslint_d,
+    null_ls.builtins.code_actions.eslint,
+    null_ls.builtins.diagnostics.eslint,
     null_ls.builtins.formatting.gofmt,
   }
 })
