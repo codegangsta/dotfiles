d5b757277be7d69e1251820e7006c03891c8508b
Jeremy Saenz <jeremy.saenz@gmail.com>
2023-08-18 18:31:35 -0700

    Lots of fun sketchybar confi

---
+++ config/sketchybar/colors.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Color Palette
+export BLACK=0xff181926
+export WHITE=0xffcad3f5
+export RED=0xffed8796
+export GREEN=0xffa6da95
+export BLUE=0xff8aadf4
+export YELLOW=0xffeed49f
+export ORANGE=0xfff5a97f
+export MAGENTA=0xffc6a0f6
+export GREY=0xff939ab7
+export TRANSPARENT=0x00000000
+
+# General bar colors
+export BAR_COLOR=0xa024273a
+export ICON_COLOR=$WHITE # Color of all icons
+export LABEL_COLOR=$WHITE # Color of all labels
+export BACKGROUND_1=0x903c3e4f
+export BACKGROUND_2=0x90494d64
+
+export POPUP_BACKGROUND_COLOR=0xff24273a
+export POPUP_BORDER_COLOR=$WHITE
+
+export SHADOW_COLOR=$BLACK
---
+++ config/sketchybar/plugins/space.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+update() {
+  WIDTH="dynamic"
+  if [ "$SELECTED" = "true" ]; then
+    WIDTH="0"
+  fi
+
+  sketchybar --animate tanh 20 --set $NAME icon.highlight=$SELECTED label.width=$WIDTH
+}
+
+mouse_clicked() {
+  if [ "$BUTTON" = "right" ]; then
+    yabai -m space --destroy $SID
+    sketchybar --trigger space_change --trigger windows_on_spaces
+  else
+    yabai -m space --focus $SID 2>/dev/null
+  fi
+}
+
+case "$SENDER" in
+  "mouse.clicked") mouse_clicked
+  ;;
+  *) update
+  ;;
+esac
