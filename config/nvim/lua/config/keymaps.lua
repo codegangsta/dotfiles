873c523e5202de934cc6f2edf14be10b1eabba44
Jeremy Saenz <jeremy.saenz@gmail.com>
2024-05-23 15:30:29 -0700

    Added keymaps for lazyvim

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = LazyVim.safe_keymap_set
+
+-- LazyGit
+map("n", "<C-g>", function()
+  LazyVim.lazygit({ cwd = LazyVim.root.git() })
+end, { desc = "Lazygit (Root Dir)" })
+map("n", "<C-b>", function() end, { desc = "Lazygit Blame line" })
