ad6de3458fca3b4e90cf7585f64956142f8a271e
Jeremy Saenz <jeremy.saenz@gmail.com>
2024-05-23 22:05:03 -0700

    Added comment

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -4,6 +4,7 @@ require("config.lazy")
 -- set colors not included in nord
 vim.api.nvim_set_hl(0, "WinSeparator", { foreground = 2698558 })
 
+-- Set the file type for astro files
 vim.filetype.add({
   extension = {
     astro = "html",
