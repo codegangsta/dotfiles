77eedc7665e327169ebbbce9ef2f6e7273e7daec
Jeremy Saenz <jeremy.saenz@gmail.com>
2024-08-16 10:23:55 -0700

    Bring back autocomplete for cmp

--- config/nvim/lua/plugins/nvim-cmp.lua
+++ config/nvim/lua/plugins/nvim-cmp.lua
@@ -18,12 +18,6 @@ return {
 
       local cmp = require("cmp")
 
-      opts.completion = {
-        autocomplete = false,
-        keyword_pattern = [[\k\+]],
-        completeopt = "menu,menuone,noselect",
-      }
-
       opts.mapping = vim.tbl_extend("force", opts.mapping, {
         ["<C-Space>"] = cmp.mapping.complete(),
         ["<Tab>"] = cmp.mapping(function(fallback)
